d4ed8236ce47af91d91465ca00a34ee3abe35f2c
Nico <lozada.nico@gmail.com>
2023-08-22 11:49:50 -0400

    Initial Commit

---
+++ tattoo-artist-finder/tattoo_artist_finderApp.swift
@@ -0,0 +1,17 @@
+//
+//  tattoo_artist_finderApp.swift
+//  tattoo-artist-finder
+//
+//  Created by Lozada, Nico on 8/22/23.
+//
+
+import SwiftUI
+
+@main
+struct tattoo_artist_finderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tattoo-artist-finderUITests/tattoo_artist_finderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  tattoo_artist_finderUITestsLaunchTests.swift
+//  tattoo-artist-finderUITests
+//
+//  Created by Lozada, Nico on 8/22/23.
+//
+
+import XCTest
+
+final class tattoo_artist_finderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
